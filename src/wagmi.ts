d3107b3721adef0dcfa5ae34e62e694934755297
Mubarak <mubaraknewt10@gmail.com>
2025-10-03 10:18:46 +0100

    update

---
+++ src/wagmi.ts
@@ -0,0 +1,41 @@
+import { cookieStorage, createStorage } from '@wagmi/core'
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
+import {
+  mainnet,
+  arbitrum,
+  avalanche,
+  bsc,
+  base,
+  optimism,
+  polygon,
+  sepolia,
+  type AppKitNetwork,
+} from '@reown/appkit/networks'
+
+export const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID
+
+if (!projectId) {
+  throw new Error('Project ID is not defined')
+}
+
+export const networks = [
+  mainnet,
+  arbitrum,
+  avalanche,
+  bsc,
+  base,
+  optimism,
+  polygon,
+  ...(process.env.NODE_ENV === 'development' ? [sepolia] : []),
+] as [AppKitNetwork, ...AppKitNetwork[]]
+
+export const wagmiAdapter = new WagmiAdapter({
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+  ssr: true,
+  networks,
+  projectId,
+})
+
+export const config = wagmiAdapter.wagmiConfig

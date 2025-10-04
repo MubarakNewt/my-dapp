2bb43f408556d73479e057b9099549e8616074a2
Mubarak <mubaraknewt10@gmail.com>
2025-10-04 01:14:14 +0100

    more update

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+// app/layout.tsx
+import React from "react"
+import "./globals.css"
+import Providers from "./providers"
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  )
+}
